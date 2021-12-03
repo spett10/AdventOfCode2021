c3b9bbbde2e97b1822ae576f6e74acd62ae8036a
SoerenP <stivhobbykniv@gmail.com>
2021-12-03 17:41:44 +0100

    day2

---
+++ AdventOfCode2021/Day2/Pilot.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2021.Day2
+{
+	class Pilot
+	{
+		private readonly List<string> _commands;
+		public Pilot(List<string> commands)
+		{
+			_commands = commands;
+		}
+
+		public int Navigate()
+		{
+			int horizontal = 0;
+			int depth = 0;
+
+			foreach(var command in _commands)
+			{
+				var (distance, direction) = ParseCommand(command);
+
+				switch (direction)
+				{
+					case Direction.Up:
+						depth += -distance;
+						break;
+					case Direction.Down:
+						depth += distance;
+						break;
+					case Direction.Forward:
+						horizontal += distance;
+						break;
+				}
+			}
+
+			return depth * horizontal; 
+		}
+
+		public int Aim()
+		{
+			int horizontal = 0;
+			int depth = 0;
+			int aim = 0;
+
+			foreach (var command in _commands)
+			{
+				var (distance, direction) = ParseCommand(command);
+
+				switch (direction)
+				{
+					case Direction.Up:
+						aim -= distance;
+						break;
+					case Direction.Down:
+						aim += distance;
+						break;
+					case Direction.Forward:
+						horizontal += distance;
+						depth += (aim * distance);
+						break;
+				}
+			}
+
+			return depth * horizontal;
+		}
+
+		private (int, Direction) ParseCommand(string command)
+		{
+			var split = command.Split(" ");
+
+			if (split.Length != 2) throw new ArgumentException($"expected format <string> <int> but split on \" \" returned length = {split.Length}'");
+
+			var parsedDirection = split[0].ToLower();
+			Direction direction;
+
+			if (parsedDirection.Equals("forward"))
+			{
+				direction = Direction.Forward;
+			}
+			else if (parsedDirection.Equals("up"))
+			{
+				direction = Direction.Up;
+			}
+			else if (parsedDirection.Equals("down"))
+			{
+				direction = Direction.Down;
+			}
+			else
+			{
+				throw new ArgumentException($"got unknown command: {parsedDirection}");
+			}
+
+			int distance = Int32.Parse(split[1]);
+
+			return (distance, direction);
+		}
+
+		private enum Direction
+		{
+			Forward,
+			Up,
+			Down
+		}
+
+
+	}
+}
--- AdventOfCode2021/Program.cs
+++ AdventOfCode2021/Program.cs
@@ -1,4 +1,5 @@
 ﻿using AdventOfCode2021.Day1;
+using AdventOfCode2021.Day2;
 using System;
 using System.Collections.Generic;
 
@@ -8,7 +9,7 @@ namespace AdventOfCode2021
 	{
 		static void Main(string[] args)
 		{
-			Day1();
+			Day2();
 		}
 
 		private static void Day1()
@@ -18,6 +19,20 @@ namespace AdventOfCode2021
 			var sonarscanner = new SonarScanner(measurements);
 			Console.WriteLine($"measurement increases = {sonarscanner.IncreaseCount()}");
 			Console.WriteLine($"sliding window increases = {sonarscanner.SumOfThreeSlidingWindowCount()}");
+		}
+
+		private static void Day2()
+		{
+			var testdata = new Input(@"Day2/testdata.txt");
+			var testCommands = testdata.GetLinesString<List<string>>();
+			var testpilot = new Pilot(testCommands);
+			Console.WriteLine($"test position = {testpilot.Navigate()}");
+
+			var input = new Input(@"Day2/input.txt");
+			var commands = input.GetLinesString<List<string>>();
+			var pilot = new Pilot(commands);
+			Console.WriteLine($"position = {pilot.Navigate()}");
+			Console.WriteLine($"aim = {pilot.Aim()}");
 		}
 	}
 }
