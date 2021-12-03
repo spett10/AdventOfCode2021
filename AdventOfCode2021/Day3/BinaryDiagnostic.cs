1fb5c3169e0a323fdf67049cf98835367b1c9b2e
SoerenP <stivhobbykniv@gmail.com>
2021-12-03 20:05:22 +0100

    first part of day three, too tired now

---
+++ AdventOfCode2021/Day3/BinaryDiagnostic.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode2021.Day3
+{
+	class BinaryDiagnostic
+	{
+		private readonly List<string> _readings;
+
+		public BinaryDiagnostic(List<string> readings)
+		{
+			_readings = readings;
+		}
+
+		public int PowerConsumption()
+		{
+			var bits = _readings.First().Length;
+			var bitCount = new int[bits];
+
+			foreach(var reading in _readings)
+			{
+				Read(reading, bitCount);
+			}
+
+			/* each index has the count of 1's for that position */
+			int bitCountVertially = _readings.Count;
+			var bitStringForMostCommon = "";
+			var bitStringForLeastCommon = "";
+
+			foreach(int ones in bitCount)
+			{
+				/* if more than half where ones, there were most 1's */
+				if(ones > bitCountVertially / 2)
+				{
+					bitStringForMostCommon += "1";
+					bitStringForLeastCommon += "0";
+				}
+				else
+				{
+					bitStringForMostCommon += "0";
+					bitStringForLeastCommon += "1";
+				}
+			}
+
+			var bitStringForMostCommonAsInteger = Convert.ToInt32(bitStringForMostCommon, 2);
+			var bitStringForLeastCommonAsInteger = Convert.ToInt32(bitStringForLeastCommon, 2);
+
+			return bitStringForLeastCommonAsInteger * bitStringForMostCommonAsInteger;
+		}
+
+		private void Read(string reading, int[] bitcount)
+		{
+			int index = 0;
+			foreach(char c in reading)
+			{
+				if (c == 49)
+				{
+					bitcount[index]++;
+				}
+				else if(c == 48)
+				{
+					// No op for now, perhaps for part 2 it will be relevant? 
+				}
+				else
+				{
+					throw new ArgumentException($"Expected '1' or '0' but found {c}");
+				}
+
+				index++;
+			}
+		}
+	}
+}
--- AdventOfCode2021/Program.cs
+++ AdventOfCode2021/Program.cs
@@ -1,5 +1,6 @@
 ﻿using AdventOfCode2021.Day1;
 using AdventOfCode2021.Day2;
+using AdventOfCode2021.Day3;
 using System;
 using System.Collections.Generic;
 
@@ -9,7 +10,7 @@ namespace AdventOfCode2021
 	{
 		static void Main(string[] args)
 		{
-			Day2();
+			Day3();
 		}
 
 		private static void Day1()
@@ -33,6 +34,19 @@ namespace AdventOfCode2021
 			var pilot = new Pilot(commands);
 			Console.WriteLine($"position = {pilot.Navigate()}");
 			Console.WriteLine($"aim = {pilot.Aim()}");
+		}
+
+		private static void Day3()
+		{
+			var testdata = new Input(@"Day3/testdata.txt");
+			var testReadings = testdata.GetLinesString<List<string>>();
+			var testDiagnostics = new BinaryDiagnostic(testReadings);
+			Console.WriteLine($"power consumption test data = {testDiagnostics.PowerConsumption()}");
+
+			var input = new Input(@"Day3/input.txt");
+			var readings = input.GetLinesString<List<string>>();
+			var diagnostics = new BinaryDiagnostic(readings);
+			Console.WriteLine($"power consumption input = {diagnostics.PowerConsumption()}");
 		}
 	}
 }
